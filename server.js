607a26210fe54615b6c5b0f89b81b953e42294f4
Gaurav Lamba <kiddo@192.168.1.10>
2024-08-25 20:51:46 +0530

    increased limit

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.options('*', cors(corsOptions));
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 });
 app.use(limiter);
 
